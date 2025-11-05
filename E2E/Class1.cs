b663ff8e398e370af4cfc2eb70f487cd16f19897
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-11-05 18:26:41 +0500

    Add new projects and unit testing framework
    Updated the solution to include new projects: Domain, Application, Infrastructure, Shared, UnitTest, and Integration. Each project is configured with .NET 8.0 SDK style, enabling implicit usings and nullable reference types.
    
    Introduced `Class1.cs` files for each project and added a `packages.config` for MSTest dependencies. The UnitTest project has been updated with necessary references, a new test class, and assembly metadata.

---
+++ E2E/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace E2E
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a43c1a28-30c0-4115-806b-e92487638593")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
